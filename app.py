7223935c4246a3e1825b6c184dea43764df4d305
dong.huo <huodon@gmail.com>
2023-03-29 17:25:45 +0800

    verify urls

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from typing import Union, Dict
+from typing import List, Union, Dict
 import os
-from huggingface_hub import hf_hub_download
+from huggingface_hub import hf_hub_download, hf_hub_url
 from modelscope.hub.api import HubApi
 import json
 import subprocess
+import asyncio
+import aiohttp
 
 
 def read_manifest():
@@ -21,8 +23,24 @@ def modelscope_hub() -> HubApi:
     return hub
 
 
+def verify_manifest(manifest):
+    async def check():
+        async with aiohttp.ClientSession() as session:
+            for it in manifest:
+                name, repo_id, files = it['name'], it['repo_id'], it['files']
+                for file in files:
+                    url = hf_hub_url(repo_id, filename=file)
+                    async with session.head(url) as response:
+                        if response.status >= 400:
+                            raise ValueError(
+                                f'File {file} is not found in https://huggingface.co/{repo_id}/tree/main, code {response.status}')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(check())
+
+
 def main():
     manifest = read_manifest()
+    verify_manifest(manifest)
     ms_hub = modelscope_hub()
     for it in manifest:
         name, repo_id, files = it['name'], it['repo_id'], it['files']
